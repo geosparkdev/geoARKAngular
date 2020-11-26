655d65dabced6a2ef6781060ed49851058c48904
Tiffany Young <tcy8v6@umsystem.edu>
2020-11-26 00:58:57 -0600

    getMapData()

--- geoArk/src/app/modeling/modeling.component.ts
+++ geoArk/src/app/modeling/modeling.component.ts
@@ -19,6 +19,26 @@ export class ModelingComponent implements OnInit {
   constructor(public http: HttpClient,private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
+    this.getMapdata();
+  }
+
+
+
+  getMapdata(){
+    const customheaders= new HttpHeaders()
+          .set('Content-Type', 'application/json');
+
+    this.http.get(environment.base_url+"5000/getpredictions", {headers: customheaders}).subscribe(
+      response=> {
+        console.log(response)
+
+
+      },
+      error => {
+        console.log(error)
+      }
+    )
+
   }
 
 }
