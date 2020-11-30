8912614c8b61cf8b6c8f35f8dcb9b118d1be9ec5
Tiffany Young <tcy8v6@umsystem.edu>
2020-11-29 22:19:39 -0600

    risk categories object

--- geoArk/src/app/categories2/categories2.component.ts
+++ geoArk/src/app/categories2/categories2.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { environment} from 'src/environments/environment';
+import { NgxSpinnerService } from 'ngx-spinner';
+
+//Third Party Packages 
+import * as L from 'leaflet';
+import * as colormap from 'colormap';
+
+//Objects
+import{risk_factors} from '../models/risk_factors';
+
+
 
 @Component({
   selector: 'app-categories2',
@@ -7,9 +20,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Categories2Component implements OnInit {
 
-  constructor() { }
+
+  public risk_factors:risk_factors;
+
+
+  constructor(public http: HttpClient,private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
+
+    this.risk_factors= new risk_factors();
+    this.risk_factors['Socioeconomic']=0;
+    this.risk_factors['Accessibility']=0;
+    this.risk_factors['Health resources']=0;
+    console.log(Object.values(this.risk_factors))
+
+
+
+    
+
   }
 
 }
---
+++ geoArk/src/app/models/risk_factors.ts
@@ -0,0 +1,10 @@
+export class risk_factors{
+    public risk_factors: any={
+        'Susceptibility':1,
+        'Transmission':1,
+        'Socioeconomic':1,
+        'Exposure':1,
+        'Accessibility':1,
+        'Health resources':1}
+    
+ }
