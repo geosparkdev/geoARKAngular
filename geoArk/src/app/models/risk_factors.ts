4d58e38da76dc06cf45f7ded3efb37d690c9003a
Tiffany Young <tcy8v6@umsystem.edu>
2020-11-30 00:02:09 -0600

    push

--- geoArk/src/app/models/risk_factors.ts
+++ geoArk/src/app/models/risk_factors.ts
@@ -1,9 +1,9 @@
 export class risk_factors{
-        public Susceptibility:any=1;
-        public Transmission:any=1;
-        public Socioeconomic:any=1;
-        public Exposure:any=1;
-        public Accessibility:any=1;
-        public Health_resources :any=1;
+        public Susceptibility:number=1;
+        public Transmission:number=1;
+        public Socioeconomic:number=1;
+        public Exposure:number=1;
+        public Accessibility:number=1;
+        public Health_resources :number=1;
     
  }
