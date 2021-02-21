8c73d7f8ab34e074bcb9eddb85d9c5fa722b998d
tiffany <GeoSPARKdev@gmail.com>
2021-02-21 00:07:54 -0600

    push

--- geoArk/src/app/categories2/categories2.component.ts
+++ geoArk/src/app/categories2/categories2.component.ts
@@ -3459,6 +3459,7 @@ export class Categories2Component implements OnInit {
         this.metadata=response[3];
 
         this.getTotalsGraph();
+        this.map();
 
   
       },
@@ -3508,7 +3509,6 @@ export class Categories2Component implements OnInit {
     this.filter_controller.filter1_min=Number(changeContext.value);
     this.filter_controller.filter1_max=Number(changeContext.highValue);
     this.updateTotalsFilter();
-    this.map();
   
   
   }
