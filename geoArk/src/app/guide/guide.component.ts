69c39e56a72a9daeab7ed505b06be2c1f5ec4b90
tiffany <GeoSPARKdev@gmail.com>
2021-02-22 00:12:07 -0600

    how to use

--- geoArk/src/app/guide/guide.component.ts
+++ geoArk/src/app/guide/guide.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GuideComponent implements OnInit {
 
+
+  public categories_selected:boolean=false;
+  public location_selected:boolean=false;
+  public models_selected:boolean=false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+
+  selectButton(selected:any){
+
+    this.categories_selected=false;
+    this.location_selected=false;
+    this.models_selected=false;
+
+    if(selected=="counties"){
+      this.location_selected=true;
+    }
+    if(selected=="categories"){
+      this.categories_selected=true;
+    }
+    if(selected=="model"){
+      this.models_selected=true;
+    }
   }
 
 }
