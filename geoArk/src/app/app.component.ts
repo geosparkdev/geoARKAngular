c77ef3fcc7e8e2ea9353acf9ba900ef40632b841
tiffany <GeoSPARKdev@gmail.com>
2021-03-02 23:25:04 -0600

    okta commented out for the time being

--- geoArk/src/app/app.component.ts
+++ geoArk/src/app/app.component.ts
@@ -10,21 +10,21 @@ export class AppComponent implements OnInit {
   title = 'geoArk';
   isAuthenticated: boolean;
   constructor(public oktaAuth: OktaAuthService, public router: Router) {
-    // Subscribe to authentication state changes
-    this.oktaAuth.$authenticationState.subscribe(
-      (isAuthenticated: boolean)  => this.isAuthenticated = isAuthenticated
-    );
+    // // Subscribe to authentication state changes
+    // this.oktaAuth.$authenticationState.subscribe(
+    //   (isAuthenticated: boolean)  => this.isAuthenticated = isAuthenticated
+    // );
   }
   async ngOnInit() {
     // Get the authentication state for immediate use
-    this.isAuthenticated = await this.oktaAuth.isAuthenticated();
+    // this.isAuthenticated = await this.oktaAuth.isAuthenticated();
   }
-  login() {
-    this.oktaAuth.loginRedirect('/profile');
-  }
-  async logout() {
-    // Terminates the session with Okta and removes current tokens.
-    await this.oktaAuth.logout();
-    this.router.navigateByUrl('/login');
-  }
+  // login() {
+  //   this.oktaAuth.loginRedirect('/profile');
+  // }
+  // async logout() {
+  //   // Terminates the session with Okta and removes current tokens.
+  //   await this.oktaAuth.logout();
+  //   this.router.navigateByUrl('/login');
+  // }
 }
