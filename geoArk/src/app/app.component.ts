0f7f6ae89ea91e80b3412ebe8fa232c2e24e1fd3
tiffany <GeoSPARKdev@gmail.com>
2021-03-02 23:30:16 -0600

    push

--- geoArk/src/app/app.component.ts
+++ geoArk/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { OktaAuthService } from '@okta/okta-angular';
+// import { OktaAuthService } from '@okta/okta-angular';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,11 +9,15 @@ import { OktaAuthService } from '@okta/okta-angular';
 export class AppComponent implements OnInit {
   title = 'geoArk';
   isAuthenticated: boolean;
-  constructor(public oktaAuth: OktaAuthService, public router: Router) {
-    // // Subscribe to authentication state changes
-    // this.oktaAuth.$authenticationState.subscribe(
-    //   (isAuthenticated: boolean)  => this.isAuthenticated = isAuthenticated
-    // );
+  // constructor(public oktaAuth: OktaAuthService, public router: Router) {
+  //   // // Subscribe to authentication state changes
+  //   this.oktaAuth.$authenticationState.subscribe(
+  //     (isAuthenticated: boolean)  => this.isAuthenticated = isAuthenticated
+  //   );
+  // }
+
+  constructor(public router: Router) {
+    
   }
   async ngOnInit() {
     // Get the authentication state for immediate use
