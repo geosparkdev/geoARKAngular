d79af65a60524dfa61cbaee89f0c4da3ad844e3c
tiffany <GeoSPARKdev@gmail.com>
2021-03-12 17:55:18 -0600

    development

--- geoArk/src/environments/environment.prod.ts
+++ geoArk/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  base_url:'https://geoark.missouri.edu:8443'
+  //base_url:'https://geoark.missouri.edu:8443'
+  base_url:'https://geoark.missouri.edu:5000'
 };
