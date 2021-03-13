995f7ddb3b40f21327629db1c25c7b43d9be0dd8
tiffany <GeoSPARKdev@gmail.com>
2021-03-12 20:33:47 -0600

    push

--- geoArk/src/app/categories2/categories2.component.ts
+++ geoArk/src/app/categories2/categories2.component.ts
@@ -3434,53 +3434,7 @@ export class Categories2Component implements OnInit {
         }
 
   }
-  // else
-  // {
-  //   for(let i=0; i<this.tot_table.length; i++){
 
-  //     let filter_val=this.filters_obj.find(e=> e['cnty_fips']===this.tot_table[i]['countyFIPS'])
-  //     console.log('HEREHEHREHRHEHRHERHEHREH***')
-  //     console.log(filter_val)
-
-  //     if((Number(filter_val[this.filter1.name]) >=this.filter1.min_value) && (Number(filter_val[this.filter1.name]) <=this.filter1.max_value))      
-      
-  //     {
-  //       let color=this.getColor(Number(this.tot_table[i].total_risk),0, 50, colors, range)
-
-  //       this.bar_bars.push({
-  //         data: [
-  //             { x:[Number(this.tot_table[i]['total_risk'])], 
-  //               y:[this.tot_table[i]['County Name']],
-  //               type: 'bar', 
-  //               orientation:'h',
-  //               //mode: 'lines', 
-  //               name:'tot cases',
-  //               marker: {color: color}
-  //             },
-  //         ],
-  //         layout: {
-  //                 width: 480, 
-  //                 height: 25,
-  //                 xaxis:{
-  //                   range: [Number(0), Number(factor_max.max)]
-  //                 },
-  //                 margin:{
-  //                   l:125, 
-  //                   r:0, 
-  //                   t:0, 
-  //                   b:0, 
-  //                   pad:0
-  //                 },
-  
-  //                 size: 6,
-  //           }
-  //       });
-  //     }
-  //   }
-  // }
-
-
-  
 
 }
 
@@ -3589,8 +3543,7 @@ export class Categories2Component implements OnInit {
     this.filter1=new filter();
     cat_geoJSON.clearLayers();
     this.spinnertogg=true;
-    this.getTotals([this.risk_factors.Accessibility,this.risk_factors.Exposure,this.risk_factors.Health_resources,this.risk_factors.Socioeconomic,this.risk_factors.Susceptibility,this.risk_factors.Transmission]);
-
+    this.getTotalsGraph();
   }
 
 
@@ -3621,27 +3574,6 @@ export class Categories2Component implements OnInit {
   }
 
 
-
-  FilterUpdate(){
-
-
-    const customheaders= new HttpHeaders()
-          .set('Content-Type', 'application/json');
-  
-    this.http.post(environment.base_url+"/FilterUpdate",JSON.stringify(this.initial_tot_table), {headers: customheaders}).subscribe(
-      response=> {
-        console.log(response)
-
-        //this.getTotalsGraph();
-
-  
-      },
-      error => {
-        console.log(error)
-      }
-    )
-  
-  }
 
 
 
@@ -3674,9 +3606,6 @@ export class Categories2Component implements OnInit {
     this.filter1.min_value=Number(changeContext.value);
     this.filter1.max_value=Number(changeContext.highValue);
 
-
-
-    
     this.filter_controller.filter1_min=Number(changeContext.value);
     this.filter_controller.filter1_max=Number(changeContext.highValue);
 
