11975a20b5d0901049a16bd15651d4a53ddc90ff
tiffany <GeoSPARKdev@gmail.com>
2021-03-25 00:37:41 -0500

    push

--- geoArk/src/app/models/risk_factors.ts
+++ geoArk/src/app/models/risk_factors.ts
@@ -1,9 +1,9 @@
 export class risk_factors{
-        public Susceptibility:Number=0;
-        public Transmission:Number=0;
-        public Socioeconomic:Number=0;
-        public Exposure:Number=0;
+        public Susceptibility:Number=11;
+        public Transmission:Number=11;
+        public Socioeconomic:Number=11;
+        public Exposure:Number=11;
         public Accessibility:Number=1;
-        public Health_resources :Number=0;
+        public Health_resources :Number=11;
     
  }
