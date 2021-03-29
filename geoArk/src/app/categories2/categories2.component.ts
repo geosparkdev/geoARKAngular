8c2fb96840810aafd52e4ca86aa761ed449f2c5b
tiffany <GeoSPARKdev@gmail.com>
2021-03-28 23:40:58 -0500

    push

--- geoArk/src/app/categories2/categories2.component.ts
+++ geoArk/src/app/categories2/categories2.component.ts
@@ -3389,7 +3389,7 @@ unselectAll(){
   if(this.filter1.toggle==false){
 
     let factor_max=this.metadata.find(e=> e['factor']===current_fact)
-    let range=this.getBins(0,factor_max.max,50)
+    let range=this.getBins(factor_max.min,factor_max.max,50)
 
     for(let i=0; i<this.initial_tot_table.length; i++){
 
@@ -3429,7 +3429,7 @@ unselectAll(){
   {
 
     let factor_max=this.metadata.find(e=> e['factor']===current_fact)
-    let range=this.getBins(0,factor_max.max,50)
+    let range=this.getBins(factor_max.min,factor_max.max,50)
 
     this.tot_table=this.initial_tot_table.filter(e=> e[this.filter1.name] >= this.filter1.min_value && e[this.filter1.name] <= this.filter1.max_value )
 
@@ -3703,7 +3703,7 @@ unselectAll(){
 
     //light to dark
       
-    for(var i=(threshold); i>=0; i--)
+    for(var i=(threshold-1); i>=0; i--)
     {
       if(temp[current_fact] >=ranges[i])
       {
