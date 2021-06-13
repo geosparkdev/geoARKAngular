affd663ded0795e7fd5ca7fef9ae3e24c12be640
tiffany <GeoSPARKdev@gmail.com>
2021-06-13 00:12:15 -0500

    push

--- geoArk/src/app/geoark/geoark.component.ts
+++ geoArk/src/app/geoark/geoark.component.ts
@@ -2,6 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { HttpHeaders } from '@angular/common/http';
 import { HttpClient } from '@angular/common/http';
 import { environment} from 'src/environments/environment';
+import * as L from 'leaflet';
+
+
+var geo_map:any;
+var geo_info:any;
+var geo_map_status:any;
+var geo_geoJSON:any;
+
+
 
 @Component({
   selector: 'app-geoark',
@@ -16,6 +25,11 @@ export class GeoarkComponent implements OnInit {
   constructor(public http: HttpClient) { }
 
   ngOnInit(): void {
+
+
+
+    geo_map_status=0;
+    geo_map = L.map("geo_map").setView([38.573936, -92.603760], 7);
 
     this.getAttributes();
   }
