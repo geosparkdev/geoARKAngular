d1308ad5aef8e706abf012c6021fcc92de274159
tiffany <GeoSPARKdev@gmail.com>
2021-06-17 20:13:11 -0500

    uscounties geojson added

--- geoArk/src/app/geoark/geoark.component.ts
+++ geoArk/src/app/geoark/geoark.component.ts
@@ -5,6 +5,10 @@ import { environment} from 'src/environments/environment';
 import * as L from 'leaflet';
 
 
+import {uscounties} from '../models/uscounties';
+
+
+
 var geo_map:any;
 var geo_info:any;
 var geo_map_status:any;
@@ -23,12 +27,19 @@ export class GeoarkComponent implements OnInit {
   public attributes:any;
   public data:any;
 
+  public countiesJSON:uscounties;
+  
+
+
+
+  
+
   constructor(public http: HttpClient) { }
 
   ngOnInit(): void {
 
 
-
+    this.countiesJSON=new uscounties()
     geo_map_status=0;
     geo_map = L.map("geo_map").setView([38.573936, -92.603760], 7);
 
@@ -83,6 +94,111 @@ export class GeoarkComponent implements OnInit {
 
 
 
+
+
+
+
+
+ 
+  map(){
+
+    var data=this.data;
+
+  
+
+
+  L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", 
+  {
+    id: "mapbox.light",
+    attribution: "SOS"
+    // can have min and max zoom here
+  }).addTo(geo_map);
+
+
+  // info block
+  geo_info = new L.Control({position: 'bottomleft'});
+  geo_info.onAdd = function () {
+    if(geo_map_status==0){
+      this._div = L.DomUtil.create("div", "info");
+      geo_map_status=1;
+    }
+    else
+    {
+
+      this._div=document.getElementsByClassName("geo_info")[0];
+
+    }
+      this.update();
+      return this._div;
+    };
+
+   //info box display
+    geo_info.update = function (props: any) {
+      this._div.innerHTML =
+        (props ? "<b>County: </b>" : "<h4>Click to see details of another county.</h4>");
+    };
+    geo_info.addTo(geo_map);
+
+
+
+  //geoJSON object and coloring of county
+  geo_geoJSON= L.geoJSON(this.countiesJSON.geoJSONcounties, {
+    style: function (feature) {
+      return{
+        color: 'black',
+        weight: 1,
+        //fillColor:getcolor(feature.id),
+        fillColor:'#ffffff',
+        fillOpacity:0.8,
+      }
+    },
+    onEachFeature: (feature, layer) => this.onEachFeature(feature, layer)
+  }).addTo(geo_map);
+
+
+
+
+
+
+}
+
+
+
+
+// map functions 
+highlightFeature(e) {
+  const layer = e.target;
+  layer.setStyle({
+    weight: 2,
+    color: "black",
+    fillOpacity: 1.0
+  });
+  if (!L.Browser.ie && !L.Browser.edge) {
+    //layer.bringToFront();
+  }
+  geo_info.update(layer.feature.properties);
+}
+
+ onEachFeature(feature, layer: L.Layer) {
+  layer.on({
+    mouseover: this.highlightFeature,
+    mouseout: this.resetHighlight,
+  });
+}
+
+resetHighlight(e) {
+  geo_geoJSON.resetStyle(e.target);
+  geo_info.update();
+}
+
+/*zoomToFeature(e) {
+  risk_map.fitBounds(e.target.getBounds());
+}*/
+
+onOutlineEachFeature(feature, layer: L.Layer) {
+  layer.on({
+  });
+} 
 
 
 
